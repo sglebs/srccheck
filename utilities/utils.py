f373c6422e09b09194ef85079aa42a9f7bf69ea8
Marcio Marchini <mqm@sglebs.com>
2016-12-08 11:08:28 -0200

    Fixes #39 - added synthetic metric CountDeclMethodNonStub. *2 on prop. count

--- utilities/utils.py
+++ utilities/utils.py
@@ -78,7 +78,7 @@ def stream_of_entity_with_metrics (entities, metrics, verbose, skipLibraries,reg
             metric_dict ["CountParams"] = len(entity.ents("Define", "Parameter"))
         if "CountDeclMethodNonStub" in metric_dict:
             extra_metrics = entity.metric(["CountDeclMethod" , "CountDeclPropertyAuto"])
-            metric_dict["CountDeclMethodNonStub"] = extra_metrics.get("CountDeclMethod",0) - extra_metrics.get("CountDeclPropertyAuto",0)
+            metric_dict["CountDeclMethodNonStub"] = extra_metrics.get("CountDeclMethod",0) - (2 * extra_metrics.get("CountDeclPropertyAuto",0)) # note we can't always multiply by 2 (not always a getter and setter) but this is the best we can do
         yield [entity, container_file, metric_dict]
 
 
