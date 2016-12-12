2f64dc35edbcf142b53a5d5bdc5d2d059f09abbe
Marcio Marchini <mqm@sglebs.com>
2016-12-12 10:05:59 -0200

    fixes #47 - mpld3.plugins.MousePosition(fmt=".2f")

--- utilities/utils.py
+++ utilities/utils.py
@@ -160,7 +160,7 @@ def save_scatter(x_values, x_label, y_values, y_label, ball_values, ball_label, 
     if format == "html":
         tooltip = mpld3.plugins.PointHTMLTooltip(scatter, labels=annotations, hoffset=10, voffset=-25)
         mpld3.plugins.connect(fig, tooltip)
-        mpld3.plugins.connect(fig, mpld3.plugins.MousePosition())
+        mpld3.plugins.connect(fig, mpld3.plugins.MousePosition(fmt=".2f"))
         mpld3.plugins.connect(fig, ClickSendToBack(scatter))
         mpld3.save_html(fig, filename)
     else:
@@ -188,7 +188,7 @@ def save_abstractness_x_instability_scatter(x_values, x_label, y_values, y_label
     plt.title("%i %s items. Circles: %s & %s" % (len(x_values), scope_name, ball_label, color_label))
     tooltip = mpld3.plugins.PointHTMLTooltip(scatter, labels=annotations, hoffset=10, voffset=-25)
     mpld3.plugins.connect(fig, tooltip)
-    mpld3.plugins.connect(fig, mpld3.plugins.MousePosition())
+    mpld3.plugins.connect(fig, mpld3.plugins.MousePosition(fmt=".2f"))
     mpld3.plugins.connect(fig, ClickSendToBack(scatter))
     filename = "%s-scatter-%s-%s_%s_%s.html" % (filename_prefix, scope_name, x_label, y_label, ball_label)
     mpld3.save_html(fig, filename)
