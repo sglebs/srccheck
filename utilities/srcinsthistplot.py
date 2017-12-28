8e346ad7b0e5679c70d63189cb09cbfccfcd3b66
Marcio Marchini <mqm@sglebs.com>
2017-12-28 17:37:03 -0200

    no need to compute normalized distance

--- utilities/srcinsthistplot.py
+++ utilities/srcinsthistplot.py
@@ -1,16 +1,14 @@
-"""Source Code OO Instability X Abstractness Plot.
+"""Source Code OO Instability X Abstractness Histogram Plot (Distance from Mean).
 
 Usage:
   srcinsthistplot   --in=<inputCSV> \r\n \
-                [--abstractnessColumn=<columnName>] \r\n \
-                [--instabilityColumn=<columnName>] \r\n \
+                [--normalizedDistanceColumn=<columnName>] \r\n \
                 [--showMeanMedian]
 
 
 Options:
-    --in=<inputCSV>                     Input CSV file path.
-    --abstractnessColumn=<columnName>   Name of the column in the CSV with the Abstractness values. [default: Abstractness]
-    --instabilityColumn=<columnName>    Name of the column in the CSV with the Imstability values. [default: Instability]
+    --in=<inputCSV>                     Input CSV file path. [default: instability.csv]
+    --normalizedDistanceColumn=<columnName>   Name of the column in the CSV with the Normalized Distance values. [default: Normalized Distance]
     -m, --showMeanMedian                If you want to show dotted lines for mean (blue) and median (red) [default: false]
 
 
@@ -23,7 +21,7 @@ import datetime
 import os
 import csv
 from docopt import docopt
-from utilities.utils import save_abstractness_x_instability_scatter
+from utilities.utils import save_histogram
 from utilities import VERSION
 import math
 
@@ -31,24 +29,21 @@ SQRT2 = math.sqrt(2.0)
 
 def hist_plot (cmdline_arguments):
     inputCSV = cmdline_arguments["--in"]
-    abstractnessColumn = cmdline_arguments["--abstractnessColumn"]
-    instabilityColumn = cmdline_arguments["--instabilityColumn"]
+    normalizedDistanceColumn = cmdline_arguments["--normalizedDistanceColumn"]
     distances_as_percentages = []
     with open(inputCSV, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            abstractness = float(row.get(abstractnessColumn,0))
-            instability = float(row.get(instabilityColumn,0))
-            distance = abs(abstractness + instability - 1) / SQRT2
-            distance_as_percentage = int (100.0 * 2.0 * distance / SQRT2)  # SQRT2/2 is the maximum possible distance, i.e. 100%
+            normalizedDistance = float(row.get(normalizedDistanceColumn,0))
+            distance_as_percentage = int (math.trunc(100.0 * normalizedDistance))
             distances_as_percentages.append(distance_as_percentage)
 
     max_value = max(distances_as_percentages) if len(distances_as_percentages) > 0 else 0
     file_name, mean, median, pstdev = save_histogram(bool(cmdline_arguments["--showMeanMedian"]),
                                                      False, # not logarythmic
-                                                     os.path.split(arguments["--in"])[-1],
+                                                     os.path.split(cmdline_arguments["--in"])[-1],
                                                      max_value,
-                                                     "% distance from AxI mean sequence",
+                                                     "distance from AxI mean sequence",
                                                      distances_as_percentages,
                                                      "Components")
     print("Saved %s" % file_name)
